50c54729e70618881b9faa2e4005f90c104bf927
hehaisheng <657935732@qq.com>
2018-10-31 09:32:33 +0800

    10.31

--- app/src/main/java/com/example/hehaisheng/testgit/MainActivity.java
+++ app/src/main/java/com/example/hehaisheng/testgit/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试再github编写
-        //其他人编写了
     }
 }
